22e8392e8cf5066d032ee830e7e2ddfdf3468b83
Terry-Rossi <terryrossi1@gmail.com>
2023-04-10 13:06:00 -0500

    initial Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http'),
+	fs = require('fs'),
+	url = require('url');
+
+http
+	.createServer((request, response) => {
+		let addr = request.url,
+			q = url.parse(addr, true),
+			filePath = '';
+
+		fs.appendFile(
+			'./log.txt',
+			`URL: ${addr}
+    Timestamp: ${new Date()}\n\n`,
+			(err) => {
+				if (err) {
+					console.error(err);
+				} else {
+					console.log('New Record added to Log File');
+				}
+			}
+		);
+		if (q.pathname.includes('documentation')) {
+			filePath = `${__dirname}/documentation.html`;
+		} else {
+			filePath = `index.html`;
+		}
+		console.log(filePath);
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				throw err;
+			}
+
+			response.writeHead(200, { 'content-type': 'text/html' });
+			response.write(data);
+			response.end();
+		});
+	})
+	.listen(8080);
+console.log('My Test Server is Running on Port 8080.');
