8a4229998ff788cca7fb4671466b516a371ed7d3
Terry-Rossi <terryrossi1@gmail.com>
2023-06-15 15:06:05 -0700

    Test update pushed to Heroku

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
 			if (allowedOrigins.indexOf(origin) === -1) {
 				// If a specific origin isn’t found on the list of allowed origins
 				let message =
-					'The CORS policy for this application doesn’t allow access from origin ' + origin;
+					'The CORS Policy for this application doesn’t allow access from origin ' + origin;
 				return done(new Error(message), false);
 			}
 			return done(null, true);
