c1f8772f143bf4782c0730aa9d976d70afc18515
Terry-Rossi <terryrossi1@gmail.com>
2023-07-21 10:26:27 -0700

    Fixed CORS issues with '/'

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ const app = express();
 const cors = require('cors');
 // app.use(cors());
 let allowedOrigins = [
-	'http://localhost:8080/',
+	'http://localhost:8080',
 	'http://localhost:1234',
 	'http://localhost:8080/movies',
 	'https://theflix-api.herokuapp.com',
-	'https://theflix-redux.netlify.app/',
+	'https://theflix-redux.netlify.app',
 ];
 console.log('TESTTTTTTTTTTTT');
 app.use(
